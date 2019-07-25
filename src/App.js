0005e4fa018eca3d4bf03538a197ab4460f88484
kiranFullStack <kiran.fullstack@outlook.com>
2019-07-25 13:59:11 +0530

    Logo and Title added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ function App() {
       <h3>React Router</h3>
       <h3>React Redux</h3>
       <h3>TypeScript</h3>
+      <hr />
+      <h3>Post request</h3>
+      <h3>Get request</h3>
+      <h3>Search</h3>
+      <h3>Sort</h3>
     </div>
   )
 }
