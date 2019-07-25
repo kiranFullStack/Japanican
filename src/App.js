d089be44891271307b2268b5faa4467aa1589382
kiranFullStack <kiran.fullstack@outlook.com>
2019-07-25 13:44:57 +0530

    technologies involved

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>Japanican Mobile and App</h1>
+      <h3>React Router</h3>
+      <h3>React Redux</h3>
+      <h3>TypeScript</h3>
     </div>
   )
 }
